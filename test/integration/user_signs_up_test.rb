90d8bdf5eb174eb7817b297193be1573a25be10f
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-11 15:55:38 -0700

    Added assertion to check that user is signed up in the db.

--- test/integration/user_signs_up_test.rb
+++ test/integration/user_signs_up_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UserSignsUpTest < ActionDispatch::IntegrationTest
 
   test "User successfully signs up" do
+    old_user_count = User.count
     visit '/users/new'
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
@@ -10,10 +11,12 @@ class UserSignsUpTest < ActionDispatch::IntegrationTest
     click_button "Create Account"
 
     assert_equal links_path, current_path
+    assert_equal old_user_count + 1, User.count
   end
 
   test "User email is already taken" do
     user = User.create(email: "test@example.com", password: "password")
+    old_user_count = User.count
     visit '/users/new'
 
     fill_in "Email", with: "test@example.com"
@@ -23,9 +26,11 @@ class UserSignsUpTest < ActionDispatch::IntegrationTest
 
     assert_equal '/users', current_path
     assert page.has_content?("Email has already been taken")
+    assert_equal old_user_count, User.count
   end
 
   test "User doesn't enter a password" do
+    old_user_count = User.count
     visit '/users/new'
 
     fill_in "Email", with: "test@example.com"
@@ -33,9 +38,11 @@ class UserSignsUpTest < ActionDispatch::IntegrationTest
 
     assert_equal '/users', current_path
     assert page.has_content?("Password can't be blank")
+    assert_equal old_user_count, User.count
   end
 
   test "User password and confirmation does not match" do
+    old_user_count = User.count
     visit '/users/new'
 
     fill_in "Email", with: "test@example.com"
@@ -45,5 +52,6 @@ class UserSignsUpTest < ActionDispatch::IntegrationTest
 
     assert_equal '/users', current_path
     assert page.has_content?("Confirmation does not match password")
+    assert_equal old_user_count, User.count
   end
 end
