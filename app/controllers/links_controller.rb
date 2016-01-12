62d344dc9abe7476c260207dc8f0b1311b186689
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-11 23:41:02 -0700

    Added functionality for user to add a link.
    Functionality was tested using functionality tests and this completes iteration 2.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,5 +2,24 @@ class LinksController < ApplicationController
   before_action :authenticate
 
   def index
+    @links = current_user.links
   end
+
+  def create
+    link = Link.new(link_params)
+    if link.save
+      current_user.links << link
+      flash[:notice] = "Link Successfully Created!"
+      redirect_to links_path
+    else
+      flash[:errors] = link.errors.full_messages
+      redirect_to links_path
+    end
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:url, :title)
+    end
 end
---
+++ test/integration/user_creates_link_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UserCreatesLinkTest < ActionDispatch::IntegrationTest
+
+  test "User successfully adds link" do
+    assert Link.count == 0
+    visit '/users/new'
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirmation", with: "password"
+    click_button "Create Account"
+    fill_in "Url", with: "https://google.com"
+    fill_in "Title", with: "My Search Page"
+    click_button "Create Link"
+
+    assert_equal links_path, current_path
+    within("#link1") do
+       assert page.has_content?("https://google.com")
+       assert page.has_content?("My Search Page")
+    end
+    assert Link.count == 1
+    assert_equal Link.last.url, "https://google.com"
+    assert_equal Link.last.title, "My Search Page"
+    assert_equal Link.last.read_status, false
+  end
+
+  test "User unsuccessfully adds link" do
+    assert Link.count == 0
+    visit '/users/new'
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirmation", with: "password"
+    click_button "Create Account"
+    click_button "Create Link"
+
+    assert_equal links_path, current_path
+    assert page.has_content?("Url is not a valid URL")
+    assert page.has_content?("Title can't be blank")
+    assert Link.count == 0
+  end
+end
