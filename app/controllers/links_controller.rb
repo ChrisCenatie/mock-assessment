ad1cf562cbb78060ef8fc06154637b245e0c9c02
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-13 15:07:12 -0700

    Links are able to be edited.
    Links are able to be edited through the links show page. This has been manually tested and still needs integration
    tests, which will be added later.
    
    The next step will be to add client side features.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -18,6 +18,21 @@ class LinksController < ApplicationController
     end
   end
 
+  def show
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    link = Link.update(params[:id], link_params)
+    if link.valid?
+      flash[:notice] = ["Link Successfully Updated!"]
+      redirect_to links_path
+    else
+      flash[:errors] = link.errors.full_messages
+      redirect_to "/links/#{params[:id]}"
+    end
+  end
+
   private
 
     def link_params
