201ab98679c7e52b630002c866c281ac0a1d8f25
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-13 20:06:04 -0700

    Added search by term functionality.

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
   $('#filter-status').on('change', filterByLinkStatus);
-})
+  $('#search-terms').on('keyup',filterBySearchTerm);
+});
+
+function filterBySearchTerm(){
+  var search = this.value;
+  var links = $('.links');
+  for (var i = 0; i < links.length; i++){
+    if(!links[i].innerText.includes(search)){
+      links[i].style.display = "none";
+    } else if (search === "") {
+      links[i].style.display = "block";
+    }
+  }
+}
 
 function filterByLinkStatus() {
   var status = this.value;
